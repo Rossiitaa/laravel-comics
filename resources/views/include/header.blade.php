abbaa7912c1e210d27809bf4d547744ae91be7a2
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-07 18:52:59 +0200

    navbar added

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,18 @@
+<div class="d-flex justify-content-center align-items-center p-3 bg-white">
+    <img class="px-5" src="../assets/images/dc-logo.png" alt="DC Logo">
+    <nav>
+        <ul>
+            <li><a href="#">characters</a></li>
+            <li><a href="#">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+            <li><input type="text" placeholder="Search"></li>
+        </ul>
+    </nav>
+</div>
